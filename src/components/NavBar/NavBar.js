d703239ab55af8441a5799005f1f7f1e82d7ca9e
gingercook <vahitaras@gmail.com>
2021-12-14 22:31:04 -0500

    menubutton component created

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,41 +1,47 @@
 import React from 'react';
+import MenuButton from '../Buttons/MenuButton';
 import classes from './NavBar.module.css';
 
 const NavBar = () => {
     const navButtons = [
         {
             key: 1,
-            name: 'Home'
+            name: 'Home',
+            link: '#Home'
         },
 
         {
             key: 2,
-            name: 'Github'
+            name: 'Github',
+            link: 'https://github.com/codecygen'
         },
 
         {
             key: 3,
-            name: 'Projects'
+            name: 'Projects',
+            link: '#Projects'
         },
 
         {
             key: 4,
-            name: 'About'
+            name: 'About',
+            link: '#About'
         },
     ];
 
     const buttons = navButtons.map(eachObject => (
-        <button
-            key={eachObject.key}>
+        <MenuButton
+            key={eachObject.key}
+            hyperlink={eachObject.link}>
             {eachObject.name}
-        </button>
+        </MenuButton>
     ));
 
     return (
         <nav className={classes.navbar}>
-            <div>Aras Sen</div>
+            <h1>Aras Sen</h1>
             <div className={classes['second-div']}>{buttons}</div>
-            <div>Logout</div>
+            <button>switch mode</button>
         </nav>
     );
 };

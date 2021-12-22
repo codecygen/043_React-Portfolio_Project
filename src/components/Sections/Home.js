dd49ba90afd23f9cb5a9b02bb6e9f4142fb19623
gingercook <vahitaras@gmail.com>
2021-12-21 21:17:41 -0500

    bouncing animation added

--- src/components/Sections/Home.js
+++ src/components/Sections/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
+
+import myImage from '../../Images/Me.jpeg';
+
 import classes from './Home.module.css';
 
 const Home = () => {
     return (
         <header className={classes.home}>
+            <img src={myImage} alt="This is my image" className={classes.myImage} />
             <h1>Aras Sen</h1>
             <h3>Full Stack Web Developer</h3>
-
-            <h1>RESUME</h1>
         </header>
     );
 };

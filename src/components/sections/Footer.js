d3391992aab80db3ec4861a98f65b6d859035aa5
gingercook <vahitaras@gmail.com>
2022-01-03 21:36:42 -0500

    footer cannot be adjusted

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,10 +2,15 @@ import React from 'react';
 import classes from './Footer.module.css';
 import SocialMedia from '../ui/SocialMedia';
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
     return (
         <footer className={`${classes.footer} footer-color`}>
-            <SocialMedia />
+            <div>
+                <SocialMedia />
+                <h4>©{currentYear} Aras Sen</h4>
+            </div>
         </footer>
     );
 };

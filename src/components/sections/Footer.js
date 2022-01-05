cffc9b3eae5223d662329f3d244b556251522d69
gingercook <vahitaras@gmail.com>
2022-01-04 22:15:11 -0500

    smooth scrolling to top is added

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,7 +2,18 @@ import React from 'react';
 import classes from './Footer.module.css';
 import SocialMedia from '../ui/SocialMedia';
 
+import { GiBroadheadArrow } from 'react-icons/gi';
+
 const currentYear = new Date().getFullYear();
+
+const scrollUp = () => {
+    
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+};
 
 const Footer = () => {
     return (
@@ -12,6 +23,7 @@ const Footer = () => {
                 <h4>Developed and designed by</h4>
                 <p className={classes['footer-decorative']}>ARAS SEN</p>
                 <h4>©{currentYear} Aras Sen</h4>
+                <GiBroadheadArrow className={classes['rotate-arrow']} onClick={scrollUp} />
             </div>
         </footer>
     );

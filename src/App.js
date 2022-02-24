0ef112b9443392e1baf39fc6b3154b471b7e7281
gingercook <vahitaras@gmail.com>
2022-02-24 01:06:07 -0500

    isVisited section added to localstorage to keep track of first time rendering

--- src/App.js
+++ src/App.js
@@ -49,9 +49,15 @@ function App() {
 
   const timeInterval = 5000;
 
+  localStorage.setItem('isVisited', 1);
+
   useEffect(() => {
 
     const interval = setInterval(() => {
+
+      if (localStorage.getItem('isVisited') === '1') {
+        console.log('hi');
+      }
 
       setLocalStorageTime(localStorage.getItem('localTime'));
 

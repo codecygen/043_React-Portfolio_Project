124b5f260e8527ed6e0547fc2d37b26964087674
gingercook <vahitaras@gmail.com>
2022-02-28 23:47:24 -0500

    project card color is being worked on

--- src/components/ui/ProjectCard.js
+++ src/components/ui/ProjectCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './ProjectCard.module.css';
 
 import Button from '../ui/buttons/Button';
 
+import DarkModeContext from '../../store/color-context';
+
 const ProjectCard = () => {
+    const darkCtx = useContext(DarkModeContext);
+
+    const projectCardColor = darkCtx.isDarkMode ? `${classes['project-card']} project-card-color-dark` : `${classes['project-card']} project-card-color-light`;
+
     return (
-        <div className={`${classes['project-card']} project-card-color`}>
+        <div className={projectCardColor}>
             <h4>This is Project</h4>
             <Button>Live</Button>
             <Button>GitHub</Button>

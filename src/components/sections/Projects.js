01b7f6ee9af7d7e1b6e58ae5780f4848482abe1e
gingercook <vahitaras@gmail.com>
2022-03-02 01:06:50 -0500

    project cards mapped instead of manually entered as different elements

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -6,17 +6,36 @@ import Divider from '../ui/Divider';
 import classes from './Projects.module.css'
 
 const Projects = () => {
+    const projectList = [
+        {
+            id: 'a1',
+            text: 'This is the project 1.',
+            liveLink: 'https://www.google.ca/',
+            githubLink: 'https://github.com/'
+        },
+
+        {
+            id: 'a2',
+            text: 'This is the project 2.',
+            liveLink: 'https://www.yahoo.ca/',
+            githubLink: 'https://twitter.com/'
+        }
+    ];
+
+    const projectCards = projectList.map(element => (
+        <ProjectCard 
+            key={element.id} 
+            text={element.text} 
+            liveLink={element.liveLink} 
+            githubLink={element.githubLink}
+        />
+    ));
+
     return (
         <section className={classes.projects} id='projects'>
             <h2>Projects</h2>
             <div className={classes['project-cards']}>
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                {projectCards}
             </div>
             <Divider />
         </section>
--- src/components/ui/ProjectCard.js
+++ src/components/ui/ProjectCard.js
@@ -5,16 +5,16 @@ import Button from '../ui/buttons/Button';
 
 import DarkModeContext from '../../store/color-context';
 
-const ProjectCard = () => {
+const ProjectCard = props => {
     const darkCtx = useContext(DarkModeContext);
 
     const projectCardColor = darkCtx.isDarkMode ? `${classes['project-card']} project-card-color-dark` : `${classes['project-card']} project-card-color-light`;
 
     return (
         <div className={projectCardColor}>
-            <h4>This is Project</h4>
-            <Button>Live</Button>
-            <Button>GitHub</Button>
+            <h4>{props.text}</h4>
+            <Button><a href={props.liveLink}>Live</a></Button>
+            <Button><a href={props.githubLink}>Github</a></Button>
         </div>
     );
 };

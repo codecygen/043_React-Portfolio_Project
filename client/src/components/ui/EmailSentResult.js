f82439a34e0e430508beec83680dce46c5c00ba5
Aras <vahitaras@gmail.com>
2023-02-14 13:47:04 -0500

    animation triggers but it does not show message

--- client/src/components/ui/EmailSentResult.js
+++ client/src/components/ui/EmailSentResult.js
@@ -1,21 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import DarkModeContext from "../../store/color-context";
+import useAssessHeight from "../../hooks/use-assessHeight";
 
 import classes from "./ContactCard.module.css";
 import classes2 from "./EmailSentResult.module.css";
 
 const EmailSentSuccess = (props) => {
-  console.log(props.elementHeight);
+  const finalHeight = useAssessHeight("warning-message");
   const darkCtx = useContext(DarkModeContext);
+
+  const [currentHeight, setCurrentHeight] = useState();
 
   const contactColor = darkCtx.isDarkMode
     ? "contact-color-dark"
     : "contact-color-light";
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentHeight(finalHeight);
+    }, 20);
+
+    setCurrentHeight(props.elementHeight);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [props.elementHeight, finalHeight]);
+
   return (
-    <section className={classes["form-card"]} id="contact">
+    <section className={classes["form-card"]} id="warning-message">
       <div className={contactColor}>
-        <h3 className={classes2.warning}>
+        <h3
+          className={classes2.warning}
+          style={{
+            height: currentHeight,
+            lineHeight: currentHeight,
+          }}
+        >
           {props.isSent ? "I received your email!" : "Failed to send email!"}
         </h3>
       </div>

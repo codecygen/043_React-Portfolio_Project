30a78b7cceca53f73f4a582f0e3f8bc174bca643
Aras <vahitaras@gmail.com>
2023-02-20 14:47:18 -0500

    updated backend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({limit: '50mb'})) // To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" })); // To parse the incoming requests with JSON payloads
 
-app.use(function(req, res, next) {
+// This section enables cors
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-const dataRoutes = require('./api');
-
+const dataRoutes = require("./api");
 
 const port = process.env.PORT || 4000;
 

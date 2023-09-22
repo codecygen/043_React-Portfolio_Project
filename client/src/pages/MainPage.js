e86f43a375f8c779dfe0919c233b35cb797a4410
Aras <vahitaras@gmail.com>
2023-09-22 14:56:39 -0400

    useVisitor custom hook is working properly

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-// import useVisitor from "../hooks/use-Visitor";
-
 import NavBar from "../components/sections/NavBar";
 import Home from "../components/sections/Home";
 import Tech from "../components/sections/Tech";
@@ -11,8 +9,6 @@ import Contact from "../components/sections/Contact";
 import Footer from "../components/sections/Footer";
 
 const AllSections = (props) => {
-
-  // useVisitor();
 
   const currentYear = new Date().getFullYear();
 

9f361d466a93c7bd776eca86661763a63be8b724
Aras <vahitaras@gmail.com>
2023-09-30 13:40:11 -0400

    visitorRoutes made leaner

--- server/routes/visitorRoutes.js
+++ server/routes/visitorRoutes.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const sendMail = require("../utils/sendMail");
-// const connectDatabase = require("../models/utils/connectDatabase");
-// const updateVisitorInfo = require("../utils/updateVisitorInfo");
-// const getMoreInfo = require("../utils/getMoreInfo");
-
 const visitorControllers = require("../controllers/visitorControllers");
 
 router.post("/visitor", visitorControllers.saveVisitorInfo);

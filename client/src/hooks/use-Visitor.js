b23509ce705d6de7c1fe3547c38be9ff6c8284a7
Aras <vahitaras@gmail.com>
2023-10-09 14:13:23 -0400

    worked on enhancing use visitor hook

--- client/src/hooks/use-Visitor.js
+++ client/src/hooks/use-Visitor.js
@@ -2,48 +2,52 @@ import { useEffect, useState } from "react";
 import checkIP from "../utils/checkIP";
 
 const useVisitor = () => {
-  const [isOKtoVisit, setIsOKtoVisit] = useState(() => {
-    const isOK = localStorage.getItem("isOKtoVisit");
-    const result = isOK !== null ? JSON.parse(isOK) : null;
-
-    return result;
-  });
+  const [isOKtoVisit, setIsOKtoVisit] = useState(null);
 
   useEffect(() => {
     const postAndGetData = async () => {
-      const ip = await checkIP();
+      // Only send request to backend if localstorage is not set
+      if (localStorage.getItem("isOKtoVisit") === null) {
+        const ip = await checkIP();
 
-      // POST DATA TO BACKEND
-      try {
-        const res = await fetch(`${process.env.REACT_APP_BACKEND}/visitor`, {
-          method: "POST",
-          body: JSON.stringify({
-            IP: ip,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        });
+        // POST DATA TO BACKEND
+        try {
+          const res = await fetch(`${process.env.REACT_APP_BACKEND}/visitor`, {
+            method: "POST",
+            body: JSON.stringify({
+              IP: ip,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+          });
 
-        if (!res.ok) {
-          console.error("Something went awry!");
+          if (!res.ok) {
+            console.error("Something went awry!");
+            return false;
+          }
+
+          const data = await res.json();
+
+          // Set data to local storage
+          localStorage.setItem("isOKtoVisit", JSON.stringify(data.isAllowed));
+
+          return data.isAllowed;
+        } catch (e) {
+          console.error(e);
           return false;
         }
-
-        const data = await res.json();
-
-        // Set data to local storage
-        localStorage.setItem("isOKtoVisit", JSON.stringify(data.isAllowed));
-
-        return data.isAllowed;
-      } catch (e) {
-        console.error(e);
-        return false;
       }
+
+      return JSON.parse(localStorage.getItem("isOKtoVisit"));
     };
-    
-    postAndGetData().then(res => {setIsOKtoVisit(res)});
+
+    // Only run the backend check if data is not set
+    // if (localStorage.getItem("isOKtoVisit") === null) {
+    postAndGetData().then((res) => {
+      setIsOKtoVisit(res);
+    });
   }, []);
 
   return isOKtoVisit;
